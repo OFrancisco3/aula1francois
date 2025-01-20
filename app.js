2393b0ed9e1f87fc3a68c309ffb49d11d9ef1607
leandroxlex1x <leandroxlex1x@gmail.com>
2025-01-20 03:25:45 +0000

    break

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+// variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un numero por favor:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces  ='vez';
 
-console.log(numeroUsuario);
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un numero por favor:");
+    
+    console.log(numeroUsuario);    
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Acertaste el numero es ${numeroSecreto} y lo hiciste en ${intentos} ${palabraVeces}	`);
+    }else{
+        if(numeroUsuario>numeroSecreto){
+            alert('El numero secreto es menor');
+        }else{
+            alert('El numero secreto es mayor')
+        }
+        intentos++;
+        palabraVeces='veces';
+        if(intentos>3){
+            alert('llegaste al maximo de intentos');
+            break;
+        }
+        console.log(intentos)
+    }
 }
--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // Add JS here
+alert('Hola mundo');
+let numeroSecreto = 6;
+let numeroUsuario = prompt('Ingresa un numero');
+console.log(numeroUsuario);
+
+if(numeroSecreto==numeroUsuario){
+    alert('Adivinaste');
+}else{
+    alert('No adivinaste');
+}
