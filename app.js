2939b77c433c577efcfa154760eb5f87c7a0debe
leandroxlex1x <leandroxlex1x@gmail.com>
2025-01-21 01:56:23 +0000

    ultimo

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // variables
-let numeroSecreto = 6;
+let numeroMaximoPosible = 10;
+let Totalnumeros = parseInt(prompt("indicar el total de numeros a adivinar"))
+let numeroSecreto = Math.round(Math.random()*numeroMaximoPosible);
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces  ='vez';
+//let palabraVeces  ='vez';
+let maximosIntentos = 6;
 
 
 while(numeroUsuario != numeroSecreto){
-    numeroUsuario = prompt("Me indicas un numero por favor:");
+    numeroUsuario = parseInt(prompt(`Me indicas un numero por favor: entre el 1 y el ${numeroMaximoPosible}`)); 
     
     console.log(numeroUsuario);    
     if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste el numero es ${numeroSecreto} y lo hiciste en ${intentos} ${palabraVeces}	`);
+        alert(`Acertaste el numero es ${numeroSecreto} y lo hiciste en ${intentos} ${intentos == 1 ? 'vez':'veces'}	`);
     }else{
         if(numeroUsuario>numeroSecreto){
             alert('El numero secreto es menor');
@@ -18,9 +21,9 @@ while(numeroUsuario != numeroSecreto){
             alert('El numero secreto es mayor')
         }
         intentos++;
-        palabraVeces='veces';
-        if(intentos>3){
-            alert('llegaste al maximo de intentos');
+        //palabraVeces='veces';        
+        if(intentos>maximosIntentos){
+            alert(`llegaste al maximo de intentos de ${maximosIntentos}`);
             break;
         }
         console.log(intentos)
